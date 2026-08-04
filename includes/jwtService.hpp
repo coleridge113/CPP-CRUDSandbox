ea3e13be961f69275cecefa5585cae207a53301d
Jose Luna <josemanuelmluna@gmail.com>
2026-08-04 18:59:15 +0800

    refactor: renamed to hpp

---
+++ includes/jwtService.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <jwt-cpp/jwt.h>
+#include <string>
+
+class JwtService 
+{
+    public:
+        std::string authenticateUser(
+                std::string& username,
+                std::string& password
+                )
+        {
+            auto token = jwt::create()
+                .set_type("JWS")
+                .set_issuer("auth0")
+                .set_payload_claim("sample", jwt::claim(std::string("test")))
+                .sign(jwt::algorithm::hs256{"secret"});
+
+            return token;
+        }
+};
