4a985e5bed570fa551baf97baeaf6ff15a491df3
Jose Luna <josemanuelmluna@gmail.com>
2026-08-04 23:16:54 +0800

    feat: added validate jwt function

--- includes/jwtService.hpp
+++ includes/jwtService.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <chrono>
+#include <iostream>
 #include <jwt-cpp/jwt.h>
 #include <string>
 
@@ -8,6 +9,8 @@ namespace JwtService
 {
 
     inline const std::string secretKey = "my-secret-key";
+    inline const std::string issuer = "crud-sandbox";
+    inline const auto signingAlgorithm = jwt::algorithm::hs256{secretKey};
 
     inline std::string authenticateUser(
             const std::string& username,
@@ -19,13 +22,32 @@ namespace JwtService
 
         auto token = jwt::create()
             .set_type("JWS")
-            .set_issuer("crud-sandbox")
+            .set_issuer(issuer)
             .set_payload_claim("name", jwt::claim(username))
             .set_payload_claim("role", jwt::claim(std::string("user")))
             .set_issued_at(issuedAt)
             .set_expires_at(expiresAt)
-            .sign(jwt::algorithm::hs256{secretKey});
+            .sign(signingAlgorithm);
 
         return token;
+    }
+
+    inline bool validateJwt(const std::string& token)
+    {
+        try
+        {
+            auto decode = jwt::decode(token);
+            auto verifier = jwt::verify()
+                .with_issuer(issuer)
+                .allow_algorithm(signingAlgorithm);
+
+            verifier.verify(decode);
+            return true;
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << "Error validating token: " << e.what() << '\n';
+            return false;
+        }
     }
 }
