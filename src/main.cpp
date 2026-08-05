429881f1e425e82db8f90657c3104a388e4a06c3
Jose Luna <josemanuelmluna@gmail.com>
2026-08-05 22:41:19 +0800

    refactor: added namespace for utils

--- src/main.cpp
+++ src/main.cpp
@@ -7,61 +7,53 @@
 
 int main() 
 {
-    try 
-    {
-        crow::SimpleApp app;
+    crow::SimpleApp app;
 
-        CROW_ROUTE(app, "/api/greet")([](const crow::request& req)
+    CROW_ROUTE(app, "/api/greet")([](const crow::request& req)
+            {
+                if (utils::isAuthenticated(req) && utils::isAuthenticated(req))
                 {
-                    if (utils::isAuthenticated(req) && utils::isAuthenticated(req))
-                    {
-                        return crow::response(200, "Hello world!");
-                    } 
-                    else 
-                    {
-                        return crow::response(401, "Unauthorized");
-                    }
-                });
-
-
-        CROW_ROUTE(app, "/api/parse").methods("POST"_method)([](const crow::request& req)
+                    return crow::response(200, "Hello world!");
+                } 
+                else 
                 {
-                    auto json = crow::json::load(req.body);
-                    if (!json || !json.has("jwt")) 
-                    {
-                        return crow::response(400, "Missing 'jwt' field in payload");
-                    }
+                    return crow::response(401, "Unauthorized");
+                }
+            });
 
-                    std::string jwtToken = json["jwt"].s();
-                    auto decoded = jwt::decode(jwtToken);
-                    return crow::response(200, decoded.get_payload());
-                });
 
-        CROW_ROUTE(app, "/api/login").methods("POST"_method)([](const crow::request& req)
+    CROW_ROUTE(app, "/api/parse").methods("POST"_method)([](const crow::request& req)
+            {
+                auto json = crow::json::load(req.body);
+                if (!json || !json.has("jwt")) 
                 {
-                    auto json = crow::json::load(req.body);
-                    if (!json || !json.has("username") || !json.has("password"))
-                    {
-                        return crow::response(400, "Missing fields");
-                    }
+                    return crow::response(400, "Missing 'jwt' field in payload");
+                }
 
-                    std::string username = json["username"].s();
-                    std::string password = json["password"].s();
+                std::string jwtToken = json["jwt"].s();
+                auto decoded = jwt::decode(jwtToken);
+                return crow::response(200, decoded.get_payload());
+            });
 
-                    auto token = JwtService::authenticateUser(username, password);
+    CROW_ROUTE(app, "/api/login").methods("POST"_method)([](const crow::request& req)
+            {
+                auto json = crow::json::load(req.body);
+                if (!json || !json.has("username") || !json.has("password"))
+                {
+                    return crow::response(400, "Missing fields");
+                }
 
-                    return crow::response(200, token);
+                std::string username = json["username"].s();
+                std::string password = json["password"].s();
 
-                });
+                auto token = JwtService::authenticateUser(username, password);
 
-        std::cout << "Server listening on http://localhost:18080" << '\n';
-        app.port(18080).multithreaded().run();
-    } 
-    catch (const std::exception& e) 
-    {
-        std::cerr << "Error occurred: " << e.what() << std::endl;
-        return 1;
-    }
+                return crow::response(200, token);
+
+            });
+
+    std::cout << "Server listening on http://localhost:18080" << '\n';
+    app.port(18080).multithreaded().run();
 
     return 0;
 }
