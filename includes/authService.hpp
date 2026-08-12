1644dc0b142b12daae0e96651c6cd47ba65b1030
Jose Luna <josemanuelmluna@gmail.com>
2026-08-12 11:17:10 +0800

    style: unindented class contents

--- includes/authService.hpp
+++ includes/authService.hpp
@@ -10,33 +10,33 @@
 
 class AuthService
 {
-    private:
-        std::shared_ptr<Database> db_;
+private:
+    std::shared_ptr<Database> db_;
 
-    public:
-        explicit AuthService(std::shared_ptr<Database> db)
-            : db_(std::move(db)) {}
+public:
+    explicit AuthService(std::shared_ptr<Database> db)
+        : db_(std::move(db)) {}
 
-        bool registerUser(
-                const std::string& username,
-                const std::string& password,
-                const std::string& role
-                ) const
-        {
-            std::string passwordHash = utils::hashPassword(password);
-            return db_->insertCredentials(username, passwordHash, role);
-        }
+    bool registerUser(
+            const std::string& username,
+            const std::string& password,
+            const std::string& role
+            ) const
+    {
+        std::string passwordHash = utils::hashPassword(password);
+        return db_->insertCredentials(username, passwordHash, role);
+    }
 
-        bool authenticateUser(
-                const std::string& username,
-                const std::string& password
+    bool authenticateUser(
+            const std::string& username,
+            const std::string& password
 
-                ) const
-        {
-            std::optional<UserCredential> userOpt = db_->getCredentials(username);
-            if (!userOpt) return false;
+            ) const
+    {
+        std::optional<UserCredential> userOpt = db_->getCredentials(username);
+        if (!userOpt) return false;
 
-            std::string passwordHash = utils::hashPassword(password);
-            return passwordHash == userOpt->passwordHash;
-        }
+        std::string passwordHash = utils::hashPassword(password);
+        return passwordHash == userOpt->passwordHash;
+    }
 };
--- includes/database.hpp
+++ includes/database.hpp
@@ -12,138 +12,138 @@ inline constexpr int TIMEOUT = 5000;
 
 class Database
 {
-    private:
-        sqlite3* db_ = nullptr;
+private:
+    sqlite3* db_ = nullptr;
 
-        void initSchema()
+    void initSchema()
+    {
+        const char* schema = R"(
+            CREATE TABLE IF NOT EXISTS user_credentials (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT NOT NULL UNIQUE,
+                password_hash TEXT NOT NULL,
+                role TEXT NOT NULL DEFAULT 'user',
+                created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+            );
+        )";
+
+        execute(schema);
+    }
+
+    void execute(const std::string& sql)
+    {
+        char* errMsg = nullptr;
+        if (sqlite3_exec(db_, sql.c_str(), nullptr, nullptr, &errMsg) != SQLITE_OK)
         {
-            const char* schema = R"(
-                CREATE TABLE IF NOT EXISTS user_credentials (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    username TEXT NOT NULL UNIQUE,
-                    password_hash TEXT NOT NULL,
-                    role TEXT NOT NULL DEFAULT 'user',
-                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-                    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
-                );
-            )";
+            std::string error = errMsg ? errMsg : "Unknown error occurred...";
+            sqlite3_free(errMsg);
+            throw std::runtime_error("SQL Execution Error: " + error);
+        }
+    }
 
-            execute(schema);
+
+public:
+    Database(const std::string& dbPath)
+    {
+        if (sqlite3_open(dbPath.c_str(), &db_) != SQLITE_OK)
+        {
+            throw std::runtime_error(
+                    "Failed to open SQLite Datbase: " + std::string(sqlite3_errmsg(db_))
+                    );
         }
 
-        void execute(const std::string& sql)
+        sqlite3_busy_timeout(db_, TIMEOUT);
+        execute("PRAGMA journal_mode = WAL");
+        execute("PRAGMA synchronous = NORMAL");
+
+        initSchema();
+    }
+
+
+    ~Database()
+    {
+        if (db_) sqlite3_close_v2(db_);
+    }
+
+    bool insertCredentials(
+            const std::string& username, 
+            const std::string& passwordHash,
+            const std::string& role
+            )
+    {
+        const char* sql = R"( 
+            INSERT INTO user_credentials (username, password_hash, role)
+            VALUES (?, ?, ?);
+        )";
+        sqlite3_stmt* stmt = nullptr;
+
+        if (sqlite3_prepare_v2(db_, sql, -1, &stmt, nullptr) != SQLITE_OK)
         {
-            char* errMsg = nullptr;
-            if (sqlite3_exec(db_, sql.c_str(), nullptr, nullptr, &errMsg) != SQLITE_OK)
-            {
-                std::string error = errMsg ? errMsg : "Unknown error occurred...";
-                sqlite3_free(errMsg);
-                throw std::runtime_error("SQL Execution Error: " + error);
-            }
+            std::cerr << "Failed to prepare statement: " << sqlite3_errmsg(db_) << std::endl;
+            return false;
         }
 
+        sqlite3_bind_text(stmt, 1, username.c_str(), -1, SQLITE_STATIC);
+        sqlite3_bind_text(stmt, 2, passwordHash.c_str(), -1, SQLITE_STATIC);
+        sqlite3_bind_text(stmt, 3, role.c_str(), -1, SQLITE_STATIC);
 
-    public:
-        Database(const std::string& dbPath)
+        int result = sqlite3_step(stmt);
+        bool success = false;
+
+        if (result == SQLITE_DONE)
         {
-            if (sqlite3_open(dbPath.c_str(), &db_) != SQLITE_OK)
-            {
-                throw std::runtime_error(
-                        "Failed to open SQLite Datbase: " + std::string(sqlite3_errmsg(db_))
-                        );
-            }
-
-            sqlite3_busy_timeout(db_, TIMEOUT);
-            execute("PRAGMA journal_mode = WAL");
-            execute("PRAGMA synchronous = NORMAL");
-
-            initSchema();
+            success = true;
+        }
+        else
+        {
+            std::cerr << "Insert failed: " << sqlite3_errmsg(db_) << std::endl;
         }
 
+        sqlite3_finalize(stmt);
 
-        ~Database()
+        return success;
+    }
+
+    const std::optional<UserCredential> getCredentials(const std::string& username)
+    {
+        const char* sql = R"(
+            SELECT * FROM user_credentials WHERE username = ?;
+        )";
+
+        sqlite3_stmt* stmt = nullptr;
+        if (sqlite3_prepare_v2(db_, sql, -1, &stmt, nullptr) != SQLITE_OK)
         {
-            if (db_) sqlite3_close_v2(db_);
+            throw std::runtime_error("Invalid credentials!");
+        }
+        sqlite3_bind_text(stmt, 1, username.c_str(), -1, SQLITE_STATIC);
+
+        int result = sqlite3_step(stmt);
+        bool success = false;
+        UserCredential user;
+
+        if (result == SQLITE_ROW)
+        {
+            success = true;
+            const unsigned char* username = sqlite3_column_text(stmt, 1);
+            const unsigned char* passwordHash = sqlite3_column_text(stmt, 2);
+
+            user.username = username 
+                ? reinterpret_cast<const char*>(username) 
+                : "";
+            user.passwordHash = passwordHash 
+                ? reinterpret_cast<const char*>(passwordHash) 
+                : "";
+        }
+        else
+        {
+            success = false;
+            std::cerr << "Failed to fetch credentials: " << sqlite3_errmsg(db_) << std::endl;
         }
 
-        bool insertCredentials(
-                const std::string& username, 
-                const std::string& passwordHash,
-                const std::string& role
-                )
-        {
-            const char* sql = R"( 
-                INSERT INTO user_credentials (username, password_hash, role)
-                VALUES (?, ?, ?);
-            )";
-            sqlite3_stmt* stmt = nullptr;
+        sqlite3_finalize(stmt);
 
-            if (sqlite3_prepare_v2(db_, sql, -1, &stmt, nullptr) != SQLITE_OK)
-            {
-                std::cerr << "Failed to prepare statement: " << sqlite3_errmsg(db_) << std::endl;
-                return false;
-            }
-
-            sqlite3_bind_text(stmt, 1, username.c_str(), -1, SQLITE_STATIC);
-            sqlite3_bind_text(stmt, 2, passwordHash.c_str(), -1, SQLITE_STATIC);
-            sqlite3_bind_text(stmt, 3, role.c_str(), -1, SQLITE_STATIC);
-
-            int result = sqlite3_step(stmt);
-            bool success = false;
-
-            if (result == SQLITE_DONE)
-            {
-                success = true;
-            }
-            else
-            {
-                std::cerr << "Insert failed: " << sqlite3_errmsg(db_) << std::endl;
-            }
-
-            sqlite3_finalize(stmt);
-
-            return success;
-        }
-
-        const std::optional<UserCredential> getCredentials(const std::string& username)
-        {
-            const char* sql = R"(
-                SELECT * FROM user_credentials WHERE username = ?;
-            )";
-
-            sqlite3_stmt* stmt = nullptr;
-            if (sqlite3_prepare_v2(db_, sql, -1, &stmt, nullptr) != SQLITE_OK)
-            {
-                throw std::runtime_error("Invalid credentials!");
-            }
-            sqlite3_bind_text(stmt, 1, username.c_str(), -1, SQLITE_STATIC);
-
-            int result = sqlite3_step(stmt);
-            bool success = false;
-            UserCredential user;
-
-            if (result == SQLITE_ROW)
-            {
-                success = true;
-                const unsigned char* username = sqlite3_column_text(stmt, 1);
-                const unsigned char* passwordHash = sqlite3_column_text(stmt, 2);
-
-                user.username = username 
-                    ? reinterpret_cast<const char*>(username) 
-                    : "";
-                user.passwordHash = passwordHash 
-                    ? reinterpret_cast<const char*>(passwordHash) 
-                    : "";
-            }
-            else
-            {
-                success = false;
-                std::cerr << "Failed to fetch credentials: " << sqlite3_errmsg(db_) << std::endl;
-            }
-
-            sqlite3_finalize(stmt);
-
-            if (success) return user;
-            return std::nullopt;
-        }
+        if (success) return user;
+        return std::nullopt;
+    }
 };
